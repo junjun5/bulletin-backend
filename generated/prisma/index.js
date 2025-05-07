6f79b147515d34ef4497ce9215951be281f2f8d4
junjun5 <jundeiiya@gmail.com>
2025-05-07 14:02:30 +0000

    modified prisma controller for all api

--- generated/prisma/index.js
+++ generated/prisma/index.js
@@ -196,11 +196,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgre:BXBHHmic4RqFT694FYeN@database-2.cbgwow8u25op.us-east-1.rds.amazonaws.com:5432/bulletin?schema=SCHEMA"
+        "value": null
       }
     }
   },
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+import { UserService } from './user.service';
+import { CategoryService } from './category.service';
+import { ThreadService } from './thread.service';
+import { PostService } from './post.service';
+import { LikeService } from './like.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, UserService, CategoryService, ThreadService, PostService, LikeService],
 })
 export class AppModule {}
