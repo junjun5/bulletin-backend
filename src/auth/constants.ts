2e6ae64e53f7ab13c72c3b26d3f854675e3c86f7
junjun5 <jundeiiya@gmail.com>
2025-06-22 11:05:57 +0000

    #3 password hashed & integrate auth

---
+++ src/auth/constants.ts
@@ -0,0 +1,6 @@
+import { SetMetadata } from '@nestjs/common';
+export const IS_PUBLIC_KEY = 'isPublic';
+export const SkipAuth = () => SetMetadata(IS_PUBLIC_KEY, true);
+export const jwtConstants = {
+  secret: 'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
+};
